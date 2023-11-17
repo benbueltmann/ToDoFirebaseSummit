6d98712fecd73c490ab48a30d62f7e1a49dafefb
Ben Bueltmann <benbueltmann@gmail.com>
2023-11-17 11:29:28 -0600

    Initial Commit

---
+++ ToDoFirebaseSummit/ToDoFirebaseSummitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoFirebaseSummitApp.swift
+//  ToDoFirebaseSummit
+//
+//  Created by Ben Bueltmann on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoFirebaseSummitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
